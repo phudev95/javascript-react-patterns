e1fc18da4b1eadc15fd4e0ec168e54ae1f315606
Phu Phan <phu.phan@zoi.de>
2022-10-27 16:36:57 +0700

    Proxy pattern

---
+++ src/javascript-patterns/04-proxy/index.js
@@ -0,0 +1,20 @@
+const person = {
+  name: "John Doe",
+  age: 42,
+  email: "john@doe.com",
+  country: "Vietnam",
+};
+
+const personProxy = new Proxy(person, {
+  get: (target, prop) => {
+    console.log(`The value of ${prop} is ${target[prop]}`);
+    return Reflect.get(target, prop);
+  },
+  set: (target, prop, value) => {
+    console.log(`Changed ${prop} from ${target[prop]} to ${value}`);
+    return Reflect.set(target, prop, value);
+  },
+});
+
+personProxy.name = 'Michael';
+console.log(personProxy.name);
